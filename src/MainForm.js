eb1ff597fb60961119ebc4e95a742cd46b2ea5f5
jasonchilcott <jasonchilcode@gmail.com>
2021-10-16 19:21:35 -0400

    added time in, out, and ndb

--- src/MainForm.js
+++ src/MainForm.js
@@ -5,6 +5,25 @@ const MainForm = () => {
   return (
     <div className="main-form">
       <h4>BG CALC Main Form</h4>
+      <form>
+      <label>Time in:</label>
+        <input
+          className="form-control"
+          type="time" id="time-in" name="time-in"
+        />
+        <label>Time out:</label>
+        <input
+          className="form-control"
+          type="time" id="time-out" name="time-out"
+        />
+        <label>NDB:</label>
+        <input
+          className="form-control"
+          type="time" id="ndb" name="ndb"
+        />
+        
+        
+      </form>
     </div>
   )
 }
