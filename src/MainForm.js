9d8913a0eff0bcc027af6036cd3fa344d7695077
jasonchilcott <jasonchilcode@gmail.com>
2021-10-16 21:42:57 -0400

    added meals section

--- src/MainForm.js
+++ src/MainForm.js
@@ -21,6 +21,48 @@ const MainForm = () => {
           className="form-control"
           type="time" id="ndb" name="ndb"
         />
+        <p>Meals</p>
+        <div>
+          <input type="radio" id="0" name="meals" value="0"
+                checked/>
+          <label for="0">0</label>
+        </div>
+        <div>
+          <input type="radio" id="1" name="meals" value="1"/>
+          <label for="1">1</label>
+        </div>
+        <div>
+          <input type="radio" id="2" name="meals" value="2"/>
+          <label for="2">2</label>
+        </div>
+        <label>1st Meal</label>
+        <input
+          className="form-control"
+          type="time" id="1st-meal" name="1st-meal"
+        />
+        <div>
+          <input type="radio" id="half" name="1st-duration" value="half"
+                checked/>
+          <label for="half">Half-hour</label>
+        </div>
+        <div>
+          <input type="radio" id="hour" name="1st-duration" value="hour"/>
+          <label for="hour">1 Hour</label>
+        </div>
+        <label>2nd Meal</label>
+        <input
+          className="form-control"
+          type="time" id="2nd-meal" name="2nd-meal"
+        />
+        <div>
+          <input type="radio" id="half" name="1st-duration" value="half"
+                checked/>
+          <label for="half">Half-hour</label>
+        </div>
+        <div>
+          <input type="radio" id="hour" name="1st-duration" value="hour"/>
+          <label for="hour">1 Hour</label>
+        </div>
         
         
       </form>
