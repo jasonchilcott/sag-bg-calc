0f7c76fe6193e67e3f45c4e936b70e98edab56f8
jasonchilcott <jasonchilcode@gmail.com>
2022-03-03 19:54:02 -0500

    redid all summary calcs, starting with OT

--- src/Summary.js
+++ src/Summary.js
@@ -18,67 +18,84 @@ const Summary = (props) => {
     return pay;
   };
 
+  // const hoursToDollarsWithNightPremiums = (int) => {
+  //   let multiplier = int.otMultiplier * (int.night ?? 1);
+  //   let tenthsTime = props.toTenths(int.toDuration().as("hours"));
+  //   return tenthsTime * (props.totalBaseRate / 8) * multiplier;
+  // };
+
   const hoursToDollars = (int) => {
-    let multiplier = int.otMultiplier * (int.night ?? 1);
+    let multiplier = int.otMultiplier;
     let tenthsTime = props.toTenths(int.toDuration().as("hours"));
     return tenthsTime * (props.totalBaseRate / 8) * multiplier;
   };
 
-  const reduceAllTimeToMoney = () => {
-    let moneyArr = props.intervals.flat(2).map((int) => {
-      return hoursToDollars(int);
-    });
-    let allHoursMoney = moneyArr.reduce((a, b) => a + b);
-    return allHoursMoney;
-  };
+  // const reduceAllTimeToMoney = () => {
+  //   let moneyArr = props.intervals.flat(2).map((int) => {
+  //     return hoursToDollars(int);
+  //   });
+  //   let allHoursMoney = moneyArr.reduce((a, b) => a + b);
+  //   return allHoursMoney;
+  // };
 
-  const totalDollars = () => {
-    if ( props.intervals ) {
-      console.log(props.intervals)
-      let intervalArray = props.intervals;
-      let hourRate = props.totalBaseRate / 8;
-      let gold = 0;
-      let bothMealPenalties =
-        mealPenaltiesPay(props.mealPenalties.first) +
-        mealPenaltiesPay(props.mealPenalties.second);
-      if (props.hoursMinusMeals.as('hours') < 8) {
-        let n1Times = intervalArray.flat(2).filter((int) => {
-          int.night === 1.1;
-        });
-        let n2Times = intervalArray.flat(2).filter((int) => {
-          int.night === 1.2;
-        });
-        let n1Dur = 0;
-        let n2Dur = 0; 
+  // const totalDollars = () => {
+  //   if ( props.intervals ) {
+  //     console.log(props.intervals)
+  //     let intervalArray = props.intervals;
+  //     let hourRate = props.totalBaseRate / 8;
+  //     let gold = 0;
+  //     let bothMealPenalties =
+  //       mealPenaltiesPay(props.mealPenalties.first) +
+  //       mealPenaltiesPay(props.mealPenalties.second);
+  //     if (props.hoursMinusMeals.as('hours') < 8) {
+  //       let n1Times = intervalArray.flat(2).filter((int) => {
+  //         int.night === 1.1;
+  //       });
+  //       let n2Times = intervalArray.flat(2).filter((int) => {
+  //         int.night === 1.2;
+  //       });
+  //       let n1Dur = 0;
+  //       let n2Dur = 0; 
 
-        if (n1Times.length) {n1Dur = props.toTenths(props.sumDurations(n1Times))}
-        if (n2Times.length) {n2Dur = props.toTenths(props.sumDurations(n2Times))}
+  //       if (n1Times.length) {n1Dur = props.toTenths(props.sumDurations(n1Times))}
+  //       if (n2Times.length) {n2Dur = props.toTenths(props.sumDurations(n2Times))}
 
-        let daytime = 8 - (n1Dur + n2Dur)
-        let shortDayMoney = hourRate * (daytime + (n1Dur * 1.1) + (n2Dur * 1.2))
-        return shortDayMoney + props.totalBumps + bothMealPenalties;
-      } else {
-        if (intervalArray[3] && intervalArray[3].ot === "gold") {
-          let goldenTime = intervalArray.pop().toDuration();
-          gold = Math.ceil(goldenTime.as("hours")) * props.totalBaseRate;
-        }
+  //       let daytime = 8 - (n1Dur + n2Dur)
+  //       let shortDayMoney = hourRate * (daytime + (n1Dur * 1.1) + (n2Dur * 1.2))
+  //       return shortDayMoney + props.totalBumps + bothMealPenalties;
+  //     } else {
+  //       if (intervalArray[3] && intervalArray[3].ot === "gold") {
+  //         let goldenTime = intervalArray.pop().toDuration();
+  //         gold = Math.ceil(goldenTime.as("hours")) * props.totalBaseRate;
+  //       }
   
-        return (Math.ceil((reduceAllTimeToMoney() + gold + props.totalBumps + bothMealPenalties) * 100) / 100).toFixed(2);
-      }
+  //       return (Math.ceil((reduceAllTimeToMoney() + gold + props.totalBumps + bothMealPenalties) * 100) / 100).toFixed(2);
+  //     }
+  //   }
+  // };
+
+  const getOt = (otType) => {
+    if (props.intervals) {
+      let hours = props.intervals.flat(2)
+      let otHours = hours.filter(interval => interval.otMultiplier === otType );
+      let otMoney = otHours.map(int => hoursToDollars(int))
+      let otDollars = otMoney.reduce(
+        (a, b) => a + b, 0);
+      return otDollars
     }
-  };
+  }
 
 
 
   return (
     <div className="summary">
       <h2>SUMMARY</h2>
-      <p>Total: $ {totalDollars()}</p>
-      <p>Base Rate: </p>
-      <p>1.5x Time: </p>
-      <p>2x Time: </p>
-      <p>Golden Time: </p>
-      <p>Night Premiums: </p>
+      <p>Total: $ </p>
+      <p>Base Rate: $ {props.totalBaseRate}</p>
+      <p>1.5x Time: $ {getOt(1.5)}</p>
+      <p>2x Time: $ {getOt(2)}</p>
+      <p>Golden Time: $ </p>
+      <p>Night Premiums: $ </p>
       <p>Lunch Penalties: $ {mealPenaltiesPay(props.mealPenalties.first).toFixed(2)}</p>
       <p>Dinner Penalties: $ {mealPenaltiesPay(props.mealPenalties.second).toFixed(2)}</p>
       <p>Wardrobe changes: </p>
